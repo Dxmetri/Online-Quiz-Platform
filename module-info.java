88f004e13309d4b42d6b5da59fa11dce2c4cce1d
Dxmetri <132477581+Dxmetri@users.noreply.github.com>
2023-05-07 19:24:56 -0400

    Delete module-info.java

--- module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxonlinequizplatform {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafxonlinequizplatform to javafx.fxml;
-    exports com.example.javafxonlinequizplatform;
-}
